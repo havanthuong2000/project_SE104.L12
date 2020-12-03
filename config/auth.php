d19db72ac29aca95d5a8844ed319e98250a0a25c
Phongpro113 <51016513+Phongpro113@users.noreply.github.com>
2020-12-03 20:09:51 +0700

    phong update

--- config/auth.php
+++ config/auth.php
@@ -91,5 +91,12 @@ return [
     |
     */
 
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],
+    ],
 
 ];
