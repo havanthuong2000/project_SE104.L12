f4ee62349d1157f1fcd8e27057ff590cd785219a
lelinh710 <74094896+lelinh710@users.noreply.github.com>
2020-12-14 20:33:44 +0700

    Update baocaodanhthu.php 2

--- app/BaoCaoDoanhThu.php
+++ app/BaoCaoDoanhThu.php
@@ -8,7 +8,7 @@ class BaoCaoDoanhThu extends Model
 {
     //
     protected $table = "baocaodoanhthu";
-   // protected $primaryKey = 'MaBCDT';
+    protected $primaryKey = 'MaBCDT';
 
     //connect chi tiet bao co doanh thu
     public function ctbcdt()
